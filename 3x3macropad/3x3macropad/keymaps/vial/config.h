faa9500401fb52860b8f41a61988939e3e417a4c
OhNoItSJoe <gedicke.johannes@googlemail.com>
2025-03-05 20:34:44 +0100

    added sample change

---
+++ 3x3macropad/3x3macropad/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x99, 0xE0, 0x5A, 0xFE, 0xFA, 0xC2, 0xA1, 0x07}
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS
+#define RGB_MATRIX_KEYPRESSES
